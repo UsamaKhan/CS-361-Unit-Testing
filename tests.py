fa6676c3e71ad188d0f5f77929a33f71eed8df21
Usama Khan <usamaarshadkhan@gmail.com>
2022-10-23 21:41:52 -0500

    Renamed test functions to comply with Python naming standards

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestInit(unittest.TestCase):
     # several of these will need to check to see if an exception is raised
-    def test_divZero(self):
+    def test_div_zero(self):
         with self.assertRaises(ZeroDivisionError, msg="Denominator of zero fails to raise DivByZero"):
             Fraction(1, 0)
 
@@ -13,13 +13,13 @@ class TestInit(unittest.TestCase):
         self.assertTrue(a.numerator == 0 & a.denominator == 1,
                         "Fails to initialize correctly with default arguments")
 
-    def test_oneArg(self):
+    def test_one_arg(self):
         arg1 = 1
         a = Fraction(arg1)
         self.assertTrue(a.numerator == arg1 & a.denominator == 1,
                         "Fails to initialize correctly with one argument")
 
-    def test_twoArg(self):
+    def test_two_arg(self):
         arg1 = 1
         arg2 = 2
         a = Fraction(arg1, arg2)
@@ -27,15 +27,15 @@ class TestInit(unittest.TestCase):
                         "Fails to initialize correctly with two arguments")
 
     # noinspection PyArgumentList
-    def test_threeArg(self):
+    def test_three_arg(self):
         with self.assertRaises(TypeError, msg="Three args provided"):
             Fraction(1, 2, 3)
 
-    def test_invalidArg(self):
+    def test_invalid_arg(self):
         with self.assertRaises(TypeError, msg="Invalid argument"):
             Fraction("hello")
 
-    def test_negDenominator(self):
+    def test_neg_denominator(self):
         arg1 = 1
         arg2 = -2
         a = Fraction(arg1, arg2)
@@ -51,17 +51,17 @@ class TestInit(unittest.TestCase):
 
 
 class TestStr(unittest.TestCase):
-    def test_displayFraction(self):
+    def test_display_fraction(self):
         a = Fraction(1, 2)
         self.assertEqual("1/2", a.__str__(),
                          "Fails to display fraction as string")
 
-    def test_displayInt(self):
+    def test_display_int(self):
         a = Fraction(2, 1)
         self.assertEqual("2", a.__str__(),
                          "Fails to display int when denominator is 1")
 
-    def test_displayNeg(self):
+    def test_display_neg(self):
         a = Fraction(-1, 2)
         self.assertEqual("-1/2", a.__str__(),
                          "Fails to display negative fraction")
